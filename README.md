90e20fc24260264967863d4f8ead4d3c8d3cbbb4
Rafal Chlodnicki <rchl2k@gmail.com>
2023-01-21 17:55:09 +0100

    resolve rome binary from workspace

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
 # LSP-rome
 
-Rome LSP server for Sublime LSP provided through [Rome](https://rome.tools/).
+[Rome](https://rome.tools/) is a unified linter and formatter for JavaScript and TypeScript files.
+
+This package enables Sublime Text integration so that you can:
+ - Format files on save or when issuing the `LSP: Format Document` command
+ - See linting hints while you type and apply code fixes
+ - Perform refactors
 
 ## Installation
 
 1. Install [LSP](https://packagecontrol.io/packages/LSP) and [LSP-rome](https://packagecontrol.io/packages/LSP-rome) via Package Control.
-2. (Optional but recommended) Install [LSP-file-watcher-chokidar](https://github.com/sublimelsp/LSP-file-watcher-chokidar) via Package Control to enable functionality to notify the server about changes to `rome.json` configuration file.
+2. (Optional but recommended) Install [LSP-file-watcher-chokidar](https://github.com/sublimelsp/LSP-file-watcher-chokidar) via Package Control to enable functionality to notify the server about changes to the `rome.json` configuration file.
 3. Restart Sublime.
 
 ### Configuration
 
 Open the configuration file using the Command Palette `Preferences: LSP-rome Settings` command or from the Sublime menu.
 
-#### languages
+## Rome Resolution
 
-Defines on which types of files the ESLint server will run.
+The package tries to use Rome from your project's local dependencies (`node_modules/rome`). We recommend adding Rome as a project dependency to ensure that NPM scripts and the extension use the same Rome version.
 
+You can also explicitly specify the `rome` binary the extension should use by configuring the `rome.lspBin` setting in `LSP-rome` Settings.
+
+If the project has no dependency on Rome and no explicit path is configured, the extension uses the Rome version managed by this package.
--- plugin.py
+++ plugin.py
@@ -1,28 +1,48 @@
 from LSP.plugin import ClientConfig, WorkspaceFolder
-from LSP.plugin.core.typing import List, Optional
+from LSP.plugin.core.typing import Dict, List, Optional
 from lsp_utils import NpmClientHandler
+import json
 import os
 import sublime
 
 
 PLATFORMS = {
     'windows': {
-        'x64': '@rometools/cli-win32-x64/rome.exe',
-        'arm64': '@rometools/cli-win32-arm64/rome.exe',
+        'x64': {
+            'binary': '@rometools/cli-win32-x64/rome.exe',
+            'package': '@rometools/cli-win32-x64'
+        },
+        'arm64': {
+            'binary': '@rometools/cli-win32-arm64/rome.exe',
+            'package': '@rometools/cli-win32-arm64'
+        },
     },
     'osx': {
-        'x64': '@rometools/cli-darwin-x64/rome',
-        'arm64': '@rometools/cli-darwin-arm64/rome',
+        'x64': {
+            'binary': '@rometools/cli-darwin-x64/rome',
+            'package': '@rometools/cli-darwin-x64'
+        },
+        'arm64': {
+            'binary': '@rometools/cli-darwin-arm64/rome',
+            'package': '@rometools/cli-darwin-arm64'
+        },
     },
     'linux': {
-        'x64': '@rometools/cli-linux-x64/rome',
-        'arm64': '@rometools/cli-linux-arm64/rome',
+        'x64': {
+            'binary': '@rometools/cli-linux-x64/rome',
+            'package': '@rometools/cli-linux-x64'
+        },
+        'arm64': {
+            'binary': '@rometools/cli-linux-arm64/rome',
+            'package': '@rometools/cli-linux-arm64'
+        },
     },
 }
 
+RESOLVED_PLATFORM = PLATFORMS.get(sublime.platform(), {}).get(sublime.arch(), {})  # type: Optional[Dict[str, str]]
 
 def resolve_platform_binary() -> Optional[str]:
-    return PLATFORMS.get(sublime.platform(), {}).get(sublime.arch())
+    return RESOLVED_PLATFORM.get('binary') if RESOLVED_PLATFORM else None
 
 
 class LspRomePlugin(NpmClientHandler):
@@ -34,12 +54,57 @@ class LspRomePlugin(NpmClientHandler):
     def is_allowed_to_start(
         cls,
         window: sublime.Window,
-        initiating_view: Optional[sublime.View] = None,
-        workspace_folders: Optional[List[WorkspaceFolder]] = None,
-        configuration: Optional[ClientConfig] = None
+        initiating_view: sublime.View,
+        workspace_folders: List[WorkspaceFolder],
+        configuration: ClientConfig
     ) -> Optional[str]:
         if not resolve_platform_binary():
             return 'LSP-rome does not support your platform currently.'
+        rome_path = cls._resolve_rome_path(workspace_folders, configuration)
+        if not rome_path:
+            return 'LSP-rome could not resolve specified rome binary.'
+        configuration.command = [rome_path, 'lsp-proxy']
+        return None
+
+    @classmethod
+    def _resolve_rome_path(cls, workspace_folders: List[WorkspaceFolder], configuration: ClientConfig) -> Optional[str]:
+        rome_lsp_bin = configuration.settings.get('rome.lspBin')
+        if isinstance(rome_lsp_bin, str) and rome_lsp_bin:
+            return cls._get_workspace_relative_path(rome_lsp_bin, workspace_folders)
+        return cls._get_workspace_dependency(workspace_folders) or '${server_path}'
+
+    @classmethod
+    def _get_workspace_relative_path(cls, rome_lsp_bin: str, workspace_folders: List[WorkspaceFolder]) -> Optional[str]:
+        if os.path.isabs(rome_lsp_bin):
+            return rome_lsp_bin
+        for folder in workspace_folders:
+            possible_path = os.path.join(folder.path, rome_lsp_bin)
+            if os.path.isfile(possible_path):
+                return possible_path
+        return None
+
+    @classmethod
+    def _get_workspace_dependency(cls, workspace_folders: List[WorkspaceFolder]) -> Optional[str]:
+        if not RESOLVED_PLATFORM:
+            return
+        package_name = RESOLVED_PLATFORM['package']
+        package_json = os.path.join(package_name, 'package.json');
+        binary_name = os.path.join(package_name, ('rome.exe' if 'win32' in package_name else 'rome'))
+        for folder in workspace_folders:
+            package_json_path = os.path.join(folder.path, 'node_modules', package_json)
+            binary_path = os.path.join(folder.path, 'node_modules', binary_name)
+            if not os.path.isfile(package_json_path) or not os.path.isfile(binary_path):
+                continue
+            try:
+                with open(package_json_path , 'r') as fp:
+                    version = json.loads(fp.read())['version']
+                    # Ignore versions lower than 0.9.0 as those didn't embed LSP server.
+                    version_tuple = tuple(map(int, (version.split('.'))))
+                    if len(version_tuple) == 3 and version_tuple < (0, 9, 0):
+                        continue
+                return binary_path
+            except:
+                continue
         return None
 
 
